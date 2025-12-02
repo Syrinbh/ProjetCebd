fb2313682a7734bd32aeac1011e65441b4683437
SyrineBH <benhassine1syrine@gmail.com>
2025-12-02 16:24:07 +0100

    modifier v0_createDB.sql

--- data/v0_createDB.sql
+++ data/v0_createDB.sql
@@ -1,29 +1,119 @@
-CREATE TABLE V0_LesSportifsEQ
-(
-  numSp NUMBER(4),
-  nomSp VARCHAR2(20),
-  prenomSp VARCHAR2(20),
-  pays VARCHAR2(20),
-  categorieSp VARCHAR2(10),
-  dateNaisSp DATE,
-  numEq NUMBER(4),
-  CONSTRAINT SP_CK1 CHECK(numSp > 0),
-  CONSTRAINT SP_CK2 CHECK(categorieSp IN ('feminin','masculin')),
-  CONSTRAINT SP_CK3 CHECK(numEq > 0)
+
+DROP TABLE IF EXISTS LesSportifs;
+DROP TABLE IF EXISTS LesEquipes;
+DROP TABLE IF EXISTS LesEpreuvesIndividuelles;
+DROP TABLE IF EXISTS LesEpreuvesParEquipe;
+DROP TABLE IF EXISTS LesMembresEquipes;
+DROP TABLE IF EXISTS LesMedaillesIndividuelle;
+DROP TABLE IF EXISTS LesMedaillesEquipe;
+DROP TABLE IF EXISTS LesInscriptionsEpreuvesIndividuelles;
+DROP TABLE IF EXISTS LesInscriptionsEpreuvesParEquipes;
+
+CREATE TABLE LesSportifs(
+idS        number(4),
+nom        VARCHAR2(20),
+prenom    VARCHAR2(20),
+date_nais    DATE,
+sexe VARCHAR2(7),
+pays    VARCHAR2(20),
+CONSTRAINT SP_PK PRIMARY KEY (idS),
+CONSTRAINT SP_U1 UNIQUE (nom, prenom),
+CONSTRAINT SP_CK1 CHECK (sexe IN ('homme','femme')),
+CONSTRAINT SP_CK2 CHECK (idS > 999),
+CONSTRAINT SP_CK3 CHECK (idS < 1501)
 );
 
-CREATE TABLE V0_LesEpreuves
+CREATE TABLE LesEquipes(
+idEq    number(4),
+CONSTRAINT EQ_PK PRIMARY KEY (idEq),
+CONSTRAINT SP_CK2 CHECK (idEq > 0),
+CONSTRAINT SP_CK2 CHECK (idEq < 101)
+);
+
+
+CREATE TABLE LesEpreuvesIndividuelles
 (
-  numEp NUMBER(3),
-  nomEp VARCHAR2(20),
-  formeEp VARCHAR2(13),
-  nomDi VARCHAR2(25),
-  categorieEp VARCHAR2(10),
-  nbSportifsEp NUMBER(2),
-  dateEp DATE,
-  CONSTRAINT EP_PK PRIMARY KEY (numEp),
-  CONSTRAINT EP_CK1 CHECK (formeEp IN ('individuelle','par equipe','par couple')),
-  CONSTRAINT EP_CK2 CHECK (categorieEp IN ('feminin','masculin','mixte')),
-  CONSTRAINT EP_CK3 CHECK (numEp > 0),
-  CONSTRAINT EP_CK4 CHECK (nbSportifsEp > 0)
+idEp    number(3),
+nom        VARCHAR2(25),
+type    VARCHAR2(7),
+dateEpi    DATE,
+discipline    VARCHAR2(25),
+CONSTRAINT EPI_PK PRIMARY KEY (idEp),
+CONSTRAINT EPI_CK CHECK (type IN ('homme', 'femme', 'mixte'))
+);
+
+CREATE TABLE LesEpreuvesParEquipe
+(
+idEp   NUMBER(4),
+nom VARCHAR2(20),
+type VARCHAR2(7),
+dateEpe    DATE,
+minsp    NUMBER(2),
+maxsp    NUMBER(2),
+forme    VARCHAR2(8),
+discipline    VARCHAR2(20),
+CONSTRAINT EPE_PK PRIMARY KEY (idEp),
+CONSTRAINT EPE_CK1 CHECK (type IN ('homme','femme','mixte')),
+CONSTRAINT EPE_CK2 CHECK (forme IN ('Equipe', 'Couple')),
+CONSTRAINT EPE_CK3 CHECK (minsp > 2),
+CONSTRAINT EPE_CK4 CHECK (minsp <= maxsp)
+);
+
+
+
+CREATE TABLE LesMembresEquipes
+(
+idEq NUMBER(4),
+idS NUMBER(4),
+CONSTRAINT MEP_PK1 PRIMARY KEY (idEq, ids),
+CONSTRAINT MEP_FK1 FOREIGN KEY (idEq) REFERENCES LesEquipes(idEq),
+CONSTRAINT MEP_FK2 FOREIGN KEY (idS) REFERENCES LesSportifs(idS)
+);
+
+CREATE TABLE LesMedaillesIndividuelle
+(
+ idEp   NUMBER(4),
+ gold     NUMBER(4),
+ argent NUMBER(4),
+ bronze NUMBER(4),
+CONSTRAINT EP_PK1 PRIMARY KEY (idEp),
+CONSTRAINT EP_CK1 CHECK (gold != argent),
+CONSTRAINT EP_CK1 CHECK (bronze != argent),
+CONSTRAINT EP_CK1 CHECK (gold != bronze),
+CONSTRAINT MEP_FK2 FOREIGN KEY (gold) REFERENCES LesSportifs(idS),
+CONSTRAINT MEP_FK2 FOREIGN KEY (argent) REFERENCES LesSportifs(idS),
+CONSTRAINT MEP_FK2 FOREIGN KEY (bronze) REFERENCES LesSportifs(idS)
+);
+
+CREATE TABLE LesMedaillesEquipe
+(
+ idEp   NUMBER(4),
+ gold     NUMBER(4),
+ argent NUMBER(4),
+ bronze NUMBER(4),
+CONSTRAINT EP_PK1 PRIMARY KEY (idEp),
+CONSTRAINT EP_CK1 CHECK (gold != argent),
+CONSTRAINT EP_CK1 CHECK (bronze != argent),
+CONSTRAINT EP_CK1 CHECK (gold != bronze),
+CONSTRAINT MEP_FK2 FOREIGN KEY (gold) REFERENCES LesEquipes(idEq),
+CONSTRAINT MEP_FK2 FOREIGN KEY (argent) REFERENCES LesEquipes(idEq),
+CONSTRAINT MEP_FK2 FOREIGN KEY (bronze) REFERENCES LesEquipes(idEq)
+);
+
+CREATE TABLE LesInscriptionsEpreuvesIndividuelles
+(
+idEp NUMBER(4),
+idS NUMBER(4),
+CONSTRAINT IEI_PK1 PRIMARY KEY (idEp, ids),
+CONSTRAINT IEI_FK1 FOREIGN KEY (idEp) REFERENCES LesEpreuvesIndividuelles(idEp),
+CONSTRAINT IEI_FK2 FOREIGN KEY (idS) REFERENCES LesSportifs(idS)
+);
+
+CREATE TABLE LesInscriptionsEpreuvesParEquipes
+(
+idEq NUMBER(4),
+idEp NUMBER(4),
+CONSTRAINT IPE_PK1 PRIMARY KEY (idEq, idEp),
+CONSTRAINT IPE_FK1 FOREIGN KEY (idEq) REFERENCES LesEquipes(idEq),
+CONSTRAINT IPE_FK2 FOREIGN KEY (idEp) REFERENCES LesEpreuvesParEquipe(idEp)
 );
